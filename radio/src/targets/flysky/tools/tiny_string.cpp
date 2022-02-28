74d1db6e24617bf14b96ed26e16afe4dda6cecc5
janek <janek.janek@gmail.com>
2022-02-28 22:28:44 +0100

    formatting

--- radio/src/targets/flysky/tools/tiny_string.cpp
+++ radio/src/targets/flysky/tools/tiny_string.cpp
@@ -4,23 +4,22 @@
 #include <cstdarg>
 
 int tiny_itoa(int number, char *arr) {
-    int r, len, digits = 1, i = 0;
+  int r, len, digits = 1, i = 0;
 
-    if (number > 99) digits++;
-    if (number > 9) digits++;
-    
-    len = digits;
+  if (number > 99) digits++;
+  if (number > 9) digits++;
+  
+  len = digits;
+  arr[i] = '0';
 
-    arr[i] = '0';
+  while (number != 0) {
+      digits--;
+      r = number % 10;
+      arr[i + digits] = r + '0';
+      number /= 10;
+  }
 
-    while (number != 0) {
-        digits--;
-        r = number % 10;
-        arr[i + digits] = r + '0';
-        number /= 10;
-    }
-
-    return len;
+  return len;
 }
 
 void tiny_sprintf(char *arr, char const *fmt, char len, char num, ...) {
@@ -28,29 +27,27 @@ void tiny_sprintf(char *arr, char const *fmt, char len, char num, ...) {
   va_start(args, num);
   char ch;
   int length = 0;
-
   int int_temp;
   char char_temp;
   char *string_temp;
 
   while ((ch = *fmt++)) {
-
     if ('%' == ch) {
       switch ((ch = *fmt++)) {
-          case 'c':
-              char_temp = va_arg(args, int);
-              arr[length] = char_temp;
-              length++;
-              break;
-          case 's':
-              string_temp = va_arg(args, char *);
-              memcpy(&arr[length], string_temp, len);
-              length += len;
-              break;
-          case 'd':
-              int_temp = va_arg(args, int);
-              length += tiny_itoa(int_temp, &arr[length]);
-              break;
+        case 'c':
+          char_temp = va_arg(args, int);
+          arr[length] = char_temp;
+          length++;
+          break;
+        case 's':
+          string_temp = va_arg(args, char *);
+          memcpy(&arr[length], string_temp, len);
+          length += len;
+          break;
+        case 'd':
+          int_temp = va_arg(args, int);
+          length += tiny_itoa(int_temp, &arr[length]);
+          break;
       }
     }
     else {
