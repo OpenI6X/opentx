8196847bbe6da5e14a811c195f336448103b5222
janek <janek.janek@gmail.com>
2022-03-19 17:47:06 +0100

    - proper address - read works

--- radio/src/targets/flysky/hal.h
+++ radio/src/targets/flysky/hal.h
@@ -82,7 +82,7 @@
 //0x40B22536; //100kHz 0x10950C27; //400kHz
 // 0x30E32E44;
 #define I2C_TIMING_400K               0x50330309; // 400kHz
-#define I2C_ADDRESS_EEPROM    0x50
+#define I2C_ADDRESS_EEPROM            0xA0 // 0x50 << 1 (convert to upper 7 bits)
 #define I2C_FLASH_PAGESIZE            64
 #define EEPROM_BLOCK_SIZE     (64)
 //#define EEPROM_VERIFY_WRITES
