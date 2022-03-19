300e4536ee7e18805fc6c3b47c608cbcfdd04e0a
janek <janek.janek@gmail.com>
2022-03-19 21:00:29 +0100

    - write works - wait for internal block write broken, replaced by hard delay

--- radio/src/targets/flysky/board.cpp
+++ radio/src/targets/flysky/board.cpp
@@ -289,6 +289,7 @@ void boardInit()
   //storageEraseAll(false);
   // TRACE("i2c test");
   // i2c_test();
+  // while (true) ;
 
 #if defined(DEBUG)
   DBGMCU_APB1PeriphConfig(DBGMCU_IWDG_STOP | DBGMCU_TIM1_STOP | DBGMCU_TIM2_STOP | DBGMCU_TIM3_STOP | DBGMCU_TIM6_STOP | DBGMCU_TIM14_STOP, ENABLE);
