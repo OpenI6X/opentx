1d9d911d19ca89c83db517f590207e92cff04b7b
Alex_SubZ <37748115+FRPProject@users.noreply.github.com>
2024-05-20 15:56:47 -0400

    else{

--- radio/src/targets/flysky/tools/inav.cpp
+++ radio/src/targets/flysky/tools/inav.cpp
@@ -60,7 +60,7 @@ struct InavData {
   int32_t currentLon;
   // uint8_t homeHeading;
   uint8_t heading;
-  int8_t MapPSign = 1;//default (+):north up|(-):north down
+  int8_t MapPSign;//default (+):north up|(-):north down
 };
 
 static InavData inavData; // = (InavData *)&reusableBuffer.cToolData[0];
@@ -145,12 +145,14 @@ static void inavDraw() {
   lcdDrawLine(LCD_W - 30, (LCD_H / 2) + FH / 2, LCD_W - 28, (LCD_H / 2) + FH / 2, DOTTED, FORCE);
 
 //MAP-Direction-Legend
-if (inavData.MapPSign>=0){ //positive N up
+if (inavData.MapPSign>0){ //positive N up
   lcdDrawText(LCD_W - 37, LCD_H/2, "E", SMLSIZE);
   lcdDrawText(LCD_W/2-1 , LCD_H-6, "S", SMLSIZE);//down
-}else{//negative north down: 180deg rotation
+}else (inavData.MapPSign<0){//negative north down: 180deg rotation
   lcdDrawText(LCD_W - 37, LCD_H/2, "W", SMLSIZE);
   lcdDrawText(LCD_W/2-1 , LCD_H-6, "N", SMLSIZE);//down
+}else{
+  inavData.MapPSign=1;
 }
 //
 
