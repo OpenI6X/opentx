8d97884ebf6d438eeb5fc3e07047753a2c86f74c
Alex_SubZ <37748115+FRPProject@users.noreply.github.com>
2024-05-19 20:20:35 -0400

    UNIT_MILLIWATTS

--- radio/src/targets/flysky/tools/inav.cpp
+++ radio/src/targets/flysky/tools/inav.cpp
@@ -261,14 +261,14 @@ static void inavDraw() {
 
   drawValueWithUnit(INAV_CURRENT_X, INAV_CURRENT_Y, current, UNIT_AMPS, PREC1 | MIDSIZE | RIGHT);
 
-  drawValueWithUnit(INAV_CURRENT_X, INAV_CURRENT_Y-10, TXPW, 'mW', MIDSIZE | RIGHT);
+  drawValueWithUnit(INAV_CURRENT_X, INAV_CURRENT_Y-10, TXPW, UNIT_MILLIWATTS, MIDSIZE | RIGHT);
 
 
   drawValueWithUnit(LCD_W - 11, 53, rssi, UNIT_DB, MIDSIZE | RIGHT);
   drawValueWithUnit(INAV_GSPD_X, INAV_GSPD_Y, speed, UNIT_KMH, PREC1 | RIGHT);
 
   drawValueWithUnit(INAV_DIST_X, INAV_DIST_Y, dist, UNIT_METERS, 0);
-  drawValueWithUnit(INAV_ALT_X, INAV_ALT_Y, alt, UNIT_METERS, RIGHT);
+  //drawValueWithUnit(INAV_ALT_X, INAV_ALT_Y, alt, UNIT_METERS, RIGHT);
 
   lcdDrawChar(INAV_SATS_X - 25, INAV_SATS_Y + 4, SATS_ICON);
   lcdDrawNumber(INAV_SATS_X, INAV_SATS_Y, sats, MIDSIZE | RIGHT);
