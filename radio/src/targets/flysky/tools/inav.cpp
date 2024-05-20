fdba6184cbc79b7360c93bc6760097356485a6a7
Alex_SubZ <37748115+FRPProject@users.noreply.github.com>
2024-05-20 16:55:49 -0400

    TINSIZE

--- radio/src/targets/flysky/tools/inav.cpp
+++ radio/src/targets/flysky/tools/inav.cpp
@@ -319,13 +319,13 @@ if (inavData.MapPSign>0){ //positive N up
 
   // lcdDrawNumber(70, 20, inavData.currentLat, SMLSIZE | RIGHT);
   // lcdDrawNumber(70, 30, inavData.currentLon, SMLSIZE | RIGHT);
-  lcdDrawNumber(INAV_ALT_X, INAV_ALT_Y, vspd, SMLSIZE | RIGHT);
+  //lcdDrawNumber(INAV_ALT_X, INAV_ALT_Y, vspd, SMLSIZE | RIGHT);
 
   static const char armText[2][5] = {    
     {'O', 'F', 'F', '\0', ' '},
     {'A', 'R', 'M', 'E', 'D'}
   };  
-  lcdDrawSizedText(INAV_DIST_X+8, INAV_DIST_Y, armText[inavData.armed], 5, SMLSIZE | RIGHT);
+  lcdDrawSizedText(INAV_DIST_X+20, INAV_DIST_Y, armText[inavData.armed], 5, TINSIZE);
 
   drawValueWithUnit(LCD_W - 6, 0, rxBatt, UNIT_VOLTS, PREC1 | RIGHT);
   drawTelemetryTopBar(); // after rxBatt to add INVERS
