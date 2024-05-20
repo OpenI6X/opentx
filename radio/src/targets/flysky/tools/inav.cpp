6851d6f8dba113f432a5faa9e4dfc0013b96d26f
Alex_SubZ <37748115+FRPProject@users.noreply.github.com>
2024-05-20 16:58:53 -0400

    INAV_BATTP_X, INAV_BATTP_Y

--- radio/src/targets/flysky/tools/inav.cpp
+++ radio/src/targets/flysky/tools/inav.cpp
@@ -325,7 +325,7 @@ if (inavData.MapPSign>0){ //positive N up
     {'O', 'F', 'F', '\0', ' '},
     {'A', 'R', 'M', 'E', 'D'}
   };  
-  lcdDrawSizedText(INAV_DIST_X+20, INAV_DIST_Y, armText[inavData.armed], 5, TINSIZE);
+  lcdDrawSizedText(INAV_BATTP_X+10, INAV_BATTP_Y, armText[inavData.armed], 5, TINSIZE);
 
   drawValueWithUnit(LCD_W - 6, 0, rxBatt, UNIT_VOLTS, PREC1 | RIGHT);
   drawTelemetryTopBar(); // after rxBatt to add INVERS
