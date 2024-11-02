43409d9c0a9ee8940d407642d6ba92883b299eed
janek <janek.janek@gmail.com>
2024-11-02 17:15:22 +0100

    definitions fix

--- radio/src/targets/common/arm/stm32/usbd_desc.c
+++ radio/src/targets/common/arm/stm32/usbd_desc.c
@@ -218,7 +218,11 @@ uint8_t *  USBD_USR_ProductStrDescriptor( uint8_t speed , uint16_t *length)
       USBD_GetString ((uint8_t*)USBD_HID_PRODUCT_FS_STRING, USBD_StrDesc, length);
       break;
 #if defined(USB_SERIAL)
+#if defined(DEBUG)
     case USB_SERIAL_MODE:
+#else
+    case USB_TELEMETRY_MIRROR_MODE:
+#endif
       USBD_GetString ((uint8_t*)USBD_CDC_PRODUCT_FS_STRING, USBD_StrDesc, length);
       break;
 #endif
@@ -272,7 +276,11 @@ uint8_t *  USBD_USR_ConfigStrDescriptor( uint8_t speed , uint16_t *length)
       USBD_GetString ((uint8_t*)USBD_HID_CONFIGURATION_FS_STRING, USBD_StrDesc, length);
       break;
 #if defined(USB_SERIAL)
+#if defined(DEBUG)
     case USB_SERIAL_MODE:
+#else
+    case USB_TELEMETRY_MIRROR_MODE:
+#endif
       USBD_GetString ((uint8_t*)USBD_CDC_CONFIGURATION_FS_STRING, USBD_StrDesc, length);
       break;
 #endif
@@ -300,7 +308,11 @@ uint8_t *  USBD_USR_InterfaceStrDescriptor( uint8_t speed , uint16_t *length)
       USBD_GetString ((uint8_t*)USBD_HID_INTERFACE_FS_STRING, USBD_StrDesc, length);
       break;
 #if defined(USB_SERIAL)
+#if defined(DEBUG)
     case USB_SERIAL_MODE:
+#else
+    case USB_TELEMETRY_MIRROR_MODE:
+#endif
       USBD_GetString ((uint8_t*)USBD_CDC_INTERFACE_FS_STRING, USBD_StrDesc, length);
       break;
 #endif
