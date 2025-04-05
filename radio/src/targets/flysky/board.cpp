bc5e65486422074893de530d01d7b245e9edeb1a
janek <janek.janek@gmail.com>
2025-04-05 23:09:08 +0200

    convert I2C, CRC and backlight to LL, fix ADC

--- radio/src/targets/flysky/board.cpp
+++ radio/src/targets/flysky/board.cpp
@@ -56,7 +56,7 @@ void SystemBootloaderJump() {
     typedef void (*pFunction)(void);
     pFunction JumpToApplication;
 
-    RCC_DeInit();
+    LL_RCC_DeInit();
 
     SysTick->CTRL = 0;
     SysTick->LOAD = 0;
