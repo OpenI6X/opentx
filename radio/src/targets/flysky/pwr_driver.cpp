ebf2812279f141ab1bdbe5a71b0250f302a12eea
janek <janek.janek@gmail.com>
2026-01-02 16:04:37 +0100

    Proper External module power pin init order

--- radio/src/targets/flysky/pwr_driver.cpp
+++ radio/src/targets/flysky/pwr_driver.cpp
@@ -29,9 +29,9 @@ void pwrInit() {
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(EXTMODULE_PWR_GPIO, &GPIO_InitStructure);
 
   GPIO_ResetBits(EXTMODULE_PWR_GPIO, EXTMODULE_PWR_GPIO_PIN);
-  GPIO_Init(EXTMODULE_PWR_GPIO, &GPIO_InitStructure);
 
   pwrOn();
 }
