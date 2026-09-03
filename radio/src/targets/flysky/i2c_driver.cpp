6c7b2e2271fdb48c00e26448c100ab325b5a6c16
janek <janek.janek@gmail.com>
2026-09-03 18:53:44 +0200

    i2c: restore pin medium speed

--- radio/src/targets/flysky/i2c_driver.cpp
+++ radio/src/targets/flysky/i2c_driver.cpp
@@ -30,7 +30,7 @@ void i2cInit()
   GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
   GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
   GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
-  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_MEDIUM;
   GPIO_InitStruct.Alternate = I2C_GPIO_AF;
   LL_GPIO_Init(I2C_GPIO, &GPIO_InitStruct);
 
