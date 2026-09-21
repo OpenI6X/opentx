fda9c8e986c0df1a3f84d9d44a3ef8483415d72f
janek <janek.janek@gmail.com>
2026-09-21 20:50:37 +0200

    serialTxBufferClear not needed for USB

--- radio/src/targets/common/arm/stm32/usb_driver.cpp
+++ radio/src/targets/common/arm/stm32/usb_driver.cpp
@@ -127,7 +127,7 @@ void usbStart()
 #if defined(USB_SERIAL)
     case USB_SERIAL_MODE:
       // initialize USB as CDC device (virtual serial port)
-      serialTxBufferClear();
+      // serialTxBufferClear();
 #if defined(STM32F0)
       USBD_Init(&USB_Device_dev, &USR_desc, &USBD_CDC_cb, &USR_cb);
 #else
